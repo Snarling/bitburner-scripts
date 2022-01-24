1e126b240a6769954ed6a56d62fbf1351a7ffd88
Snarling <84951833+Snarling@users.noreply.github.com>
2022-01-23 21:58:08 -0500

    Update unlock.js

--- unlock.js
+++ unlock.js
@@ -1,1 +1,1 @@
-export let main=(n,a=(s,p)=>n.scan(s).forEach(v=>v!=p?a(v,s):[n.brutessh,n.ftpcrack,n.relaysmtp,n.sqlinject,n.httpworm,n.nuke].forEach(p=>{try{p(s)}catch{}})))=>a("home");
+export let main=(n,a=(s,p)=>n.scan(s).forEach(v=>v!=p?a(v,s):[n.brutessh,n.ftpcrack,n.relaysmtp,n.sqlinject,n.httpworm,n.nuke].forEach(p=>{try{p(s)}catch{}})))=>a("home")
