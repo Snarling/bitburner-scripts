d212efd0b7b5c3468735f7063dea9956ef824684
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-25 13:53:13 -0400

    Update scan.js

--- libawait/awaitees/scan.js
+++ libawait/awaitees/scan.js
@@ -1,6 +1,6 @@
 //when ran via an awaiter, returns a scan of all servers.
 import {getAwaitee} from "/lib/await.js"
-export let main=(ns)=>{
+export let main=ns=>{
   let awaitee=getAwaitee(ns);
   
   let s=new Set(".");s.forEach(k=>ns.scan(k).map(s.add,s)),awaitee.returnVal=[...s];
