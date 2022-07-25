1dfc064e794a9949c7a395afd35af3d2d1c322aa
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-25 14:36:06 -0400

    Update and rename sleep3000.js to sleep.js

--- libawait/awaitees/sleep.js
+++ libawait/awaitees/sleep.js
@@ -1,5 +1,5 @@
 import {getAwaitee} from "/lib/await.js"
 export let main=async ns=>{
   getAwaitee(ns); //We don't need to save a reference to it if we're not returning a value
-  await ns.sleep(3000);
+  await ns.sleep(ns.args[0]);
 };
