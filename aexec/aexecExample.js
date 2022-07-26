338cf3017f21b3c1e6cdc201d48c725501ba9029
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-26 01:57:09 -0400

    Update and rename libawait/exampleAwaiter.js to aexec/aexecExample.js

---
+++ aexec/aexecExample.js
@@ -0,0 +1,23 @@
+import {wrapExec} from "/lib/await.js";
+export async function main(ns){
+  let aExec=wrapExec(ns);
+  
+  let time=performance.now();
+  
+  //awaiting a script without a return value
+  await aExec("/aexec/sleep.js");
+  
+  //normal way to use a script's return value
+  let number3 = await aExec("/aexec/returnNumber.js","home",1,3));
+  
+  //return value can also be used for a then-chain.
+  let servers=await aExec("/awaitees/scan.js")
+    .then(hosts=>aExec("/awaitees/getServers.js","home",1,...hosts));
+  
+  time=performance.now()-time;
+  
+  ns.tprint(`Script chain of 4 scripts took ${time}ms to complete (includes a 3000ms sleep)`);
+  ns.tprint(`${number3}.`);
+  ns.tail();
+  ns.print(servers);
+}
