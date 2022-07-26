0d3ac36485eed9232bd32d3cf5ff86ed6eb6c855
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-26 02:27:13 -0400

    Update and rename libawait/awaitees/sleep.js to aexec/sleep.js

--- libawait/awaitees/sleep.js
+++
@@ -1,5 +0,0 @@
-import {getAwaitee} from "/lib/await.js"
-export let main=async ns=>{
-  getAwaitee(ns); //We don't need to save a reference to it if we're not returning a value
-  await ns.sleep(ns.args[0]);
-};
