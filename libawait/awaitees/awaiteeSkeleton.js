e25ff1b3fe605dd1ce7dfb44ff177ce1251c2595
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-26 01:59:49 -0400

    Update and rename libawait/awaitees/awaiteeSkeleton.js to aexec/skeleton.js

--- libawait/awaitees/awaiteeSkeleton.js
+++
@@ -1,8 +0,0 @@
-import {getAwaitee} from "/lib/await.js"
-export let main=async ns=>{
-  let awaitee=getAwaitee(ns);
-  
-  //normal script code goes here
-
-  awaitee.returnVal="";//Whatever you want to return
-}
