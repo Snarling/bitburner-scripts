685f926b6b8a8e4f04eb94a292d3d7fdce0bf123
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-26 02:28:20 -0400

    Update getServers.js

--- aexec/getServers.js
+++ aexec/getServers.js
@@ -1,3 +1,3 @@
 //Written as a super-condensed main function just as an example. Other example scripts are in a more normal structure.
-import {getID} from "/lib/await.js"
+import {getID} from "/aexec/aexec.js";
 export let main=ns=>getID(ns).returnVal=ns.args.map(ns.getServer);
