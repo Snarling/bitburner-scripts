71ab8515b7860e3a0f65bf3f36a4d663cdeaf57f
Snarling <84951833+Snarling@users.noreply.github.com>
2022-07-26 02:27:41 -0400

    Update scan.js

--- aexec/scan.js
+++ aexec/scan.js
@@ -1,4 +1,4 @@
-import {getID} from "/lib/await.js"
+import {getID} from "/aexec/aexec.js";
 export let main=ns=>{
   //getID doesn't need to be called right at the beginning, but it must be called before the end of the script and ns.args will have an extra entry until it is ran.
   let s=new Set(".");s.forEach(k=>ns.scan(k).map(s.add,s)),getID(ns).returnVal=[...s];
