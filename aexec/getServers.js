23ddaa830d83a3bd61c34738a6e69ce932a83293
Snarling <84951833+Snarling@users.noreply.github.com>
2022-08-04 19:37:26 -0400

    v1

--- aexec/getServers.js
+++ aexec/getServers.js
@@ -1,6 +1,6 @@
-//Take list of server names as args, return array of associated server objects
+//Take array of server names as the first argument, return array of associated server objects
 import {getID} from "/aexec/aexec.js";
 export let main=ns=>{
   let id=getID(ns);
-  id.returnVal=id.args.map(ns.getServer);
+  id.returnVal=id.args[0].map(ns.getServer);
 }
